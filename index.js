1c98910999f20a4e1372213b38348eb4b44f7085
Rohan-Prajapati-2000 <rohanpra2000@gmail.com>
2025-03-13 23:28:33 +0530

    setup env file

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const db = require('./db');
 const Person = require('./models/person');
 const MenuItem = require('./models/menu');
 const bodyParser = require("body-parser");
+require('dotenv').config();
 
 // Middleware to parse JSON
 app.use(bodyParser.json());
 
 // Middleware to parse X-www-form-urlencoded data
 app.use(bodyParser.urlencoded({extended : true}));
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res)=>{
     res.send("Hello Welcome to my hotel")
@@ -21,6 +24,6 @@ const menuRoutes = require('./routes/menuRoute');
 app.use('/person', personRoutes);
 app.use('/menu', menuRoutes);
 
-app.listen(8000, ()=> {
-    console.log("Server started at 8000")
+app.listen(PORT, ()=> {
+    console.log(`Server started at ${PORT}`)
 })
